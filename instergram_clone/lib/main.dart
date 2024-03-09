2b84404c72e2ae132041b30d67f693efd15e64c3
Kavinda <rdks.kavinda@gmail.com>
2024-03-09 20:33:26 +0530

    Connect Firebase using fluterfirecli

--- instergram_clone/lib/main.dart
+++ instergram_clone/lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:instergram_clone/firebase_options.dart';
 import 'package:instergram_clone/resposive/mobileScreenLayout.dart';
 import 'package:instergram_clone/resposive/resposive.dart';
 import 'package:instergram_clone/resposive/webScreenLayout.dart';
 import 'package:instergram_clone/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
